140fb96d0c02362a4062839c498a7efedb191ddb
Marc-Antoine Duhaime <ma@duhaime.ca>
2024-08-21 15:01:46 -0400

    Should not be readonly

--- src/lib/CollectableTypes/index.d.ts
+++ src/lib/CollectableTypes/index.d.ts
@@ -1,11 +1,11 @@
 type ByType<T, Value> = {
-  readonly [P in keyof T as T[P] extends Value | undefined ? P : never]: T[P];
+  [P in keyof T as T[P] extends Value | undefined ? P : never]: T[P];
 };
 
 type Nullable<T> = {
-  readonly [P in keyof T as null extends T[P] ? P : never]: T[P];
+  [P in keyof T as null extends T[P] ? P : never]: T[P];
 };
 
 type Optional<T> = {
-  readonly [P in keyof T as undefined extends T[P] ? P : never]: T[P];
+  [P in keyof T as undefined extends T[P] ? P : never]: T[P];
 };
